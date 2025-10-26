6858a6b19064de30d0333e25b8e9a400a6e44856
ivan.palacios <ivan.palacios@lkmx.io>
2025-10-26 15:29:37 -0600

    refactor: update Button component styles for improved aesthetics and consistency across variants

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -17,14 +17,14 @@ export default function Button({
     variants: {
       variant: {
         primary:
-          "pointer-events-auto inline-flex items-center gap-2 rounded-[60px] border px-4 py-2 font-mono uppercase transition-colors bg-white text-black border-gray-400",
+          "mt-10 bg-black border border-[#808080] px-4 py-[0.4em] rounded-full font-mono uppercase inline-flex items-center gap-[5px] transition-colors duration-300 ease-in-out text-white",
         secondary:
-          "pointer-events-auto inline-flex items-center gap-2 rounded-[60px] border px-4 py-2 font-mono uppercase transition-colors bg-black text-white border-gray-500",
+          "mt-10 border border-[#808080] px-4 py-[0.4em] rounded-full font-mono uppercase inline-flex items-center gap-[5px] transition-colors duration-300 ease-in-out bg-black text-white hover:bg-white hover:text-black",
         outline:
           "rounded-md  border border-white bg-black px-3 py-1 font-mono uppercase backdrop-blur-md text-white transition-colors hover:bg-white hover:text-black text-[12px]",
         signature:
           "rounded-[60px] border border-white/20 bg-black/20 px-4 py-2 font-mono uppercase backdrop-blur-md transition-colors hover:bg-white hover:text-black",
-        blur: "pointer-events-auto inline-flex items-center gap-2 rounded-[60px] border border-white/20 bg-black/20 px-4 py-2 font-mono uppercase backdrop-blur-md transition-colors hover:bg-black/40",
+        blur: "mt-5 text-xs font-mono font-normal uppercase text-white border border-white/20 bg-black/20 backdrop-blur-[5px] rounded-[7px] px-4 py-[0.2em] pb-[0.5em] cursor-pointer transition-colors duration-300 hover:bg-white hover:text-black",
       },
     },
   });
