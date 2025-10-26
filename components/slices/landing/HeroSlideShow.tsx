bd688a2d311bf975a2888b22b4e79e8db3cb5660
ivan.palacios <ivan.palacios@lkmx.io>
2025-10-26 15:38:39 -0600

    refactor: remove redundant class from HeroSlideShow component for cleaner markup

--- components/slices/landing/HeroSlideShow.tsx
+++ components/slices/landing/HeroSlideShow.tsx
@@ -134,7 +134,7 @@ export default function HeroSlideShow({ slice }: SliceComponentProps) {
 
         <div className="absolute inset-0 bg-black/40" />
 
-        <div className=" absolute inset-0 flex flex-col items-center justify-center text-center px-6">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6">
           {titleEyebrow ? <Heading4 variant="eyebrow">{titleEyebrow}</Heading4> : null}
           {title ? <Heading1 variant="hero">{title}</Heading1> : null}
 
