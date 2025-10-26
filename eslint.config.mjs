05f7cb07338343008b08428fde793b86354d2055
ivan.palacios <ivan.palacios@lkmx.io>
2025-10-26 12:52:21 -0600

    chore: add Prettier and ESLint configuration files, update package dependencies, and enhance linting and formatting scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,14 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    plugins: ["prettier", "import"],
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: ["./tsconfig.json"],
+        },
+      },
+    },
+    rules: {
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      // You can add more rules here or in the overrides section below
+    },
+  }),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
 ];
 
