df26173165ce7278fd7a44e8b938173e4d9f4df4
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-09 14:45:24 -0600

    feat: implement SortMenu component with styles and functionality for sorting options in the Directory

--- components/slices/landing/Directory/Directory/SortMenu/SortMenu.tsx
+++ components/slices/landing/Directory/Directory/SortMenu/SortMenu.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import CheckIcon from "@/components/svg/check.svg";
+import ChevronIcon from "@/components/svg/chevron.svg";
+import { useEffect, useRef, useState } from "react";
+
 import "./SortMenu.scss";
 
 interface SortMenuProps {
@@ -9,16 +13,57 @@ interface SortMenuProps {
 }
 
 export default function SortMenu({ value, onChange, options }: SortMenuProps) {
+  const [optionsOpen, setOptionsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Click outside handler
+  useEffect(() => {
+    if (!optionsOpen) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setOptionsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [optionsOpen]);
+
+  const handleInput = (option: string) => {
+    onChange(option);
+    setOptionsOpen(false);
+  };
+
   return (
-    <div className="sort-menu">
-      {/* TODO: Implement sort menu */}
-      <select value={value} onChange={(e) => onChange(e.target.value)}>
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div className={`sort-menu ${optionsOpen ? "open" : ""}`} ref={containerRef}>
+      <button type="button" onClick={() => setOptionsOpen(!optionsOpen)}>
+        <span>{value}</span>
+        <ChevronIcon className="svg-chevron" />
+      </button>
+
+      <div className={`option-wrap ${optionsOpen ? "open" : ""}`}>
+        <ul className="ul-reset">
+          {options.map((option, i) => (
+            <li key={option}>
+              <input
+                id={`sortOption${i}`}
+                type="radio"
+                value={option}
+                name="sort"
+                checked={value === option}
+                onChange={() => handleInput(option)}
+              />
+              <label htmlFor={`sortOption${i}`} className={value === option ? "active" : ""}>
+                <CheckIcon className="svg-check" />
+                <span>{option}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
