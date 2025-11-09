032860b9016084422217d19c4fd94717b517ca5d
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-09 14:47:41 -0600

    feat: enhance FilterButton component with new styles and functionality for collaborator display and improved layout

--- components/slices/landing/Directory/Directory/FilterButton/FilterButton.tsx
+++ components/slices/landing/Directory/Directory/FilterButton/FilterButton.tsx
@@ -1,17 +1,41 @@
 "use client";
 
+import CloseIcon from "@/components/svg/close.svg";
+import Web3Avatar from "@/components/web3/Web3Avatar/Web3Avatar";
+import Web3Username from "@/components/web3/Web3Username/Web3Username";
+
 import "./FilterButton.scss";
 
 interface FilterButtonProps {
+  address?: string;
   className?: string;
   onClick: () => void;
   children: React.ReactNode;
 }
 
-export default function FilterButton({ className = "", onClick, children }: FilterButtonProps) {
+export default function FilterButton({
+  address,
+  className = "",
+  onClick,
+  children,
+}: FilterButtonProps) {
+  const isCollaborator = !!address;
+
   return (
-    <button className={`filter-button ${className}`} onClick={onClick} type="button">
-      {children}
+    <button
+      className={`filter-button mono ${isCollaborator ? "collaborator" : ""} ${className}`}
+      onClick={onClick}
+      type="button">
+      {address && (
+        <>
+          <Web3Avatar address={address} />
+          <Web3Username address={address} />
+        </>
+      )}
+
+      <span>{children}</span>
+
+      <CloseIcon />
     </button>
   );
 }
