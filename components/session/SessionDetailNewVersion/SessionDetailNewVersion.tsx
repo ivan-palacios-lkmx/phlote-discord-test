838ffa571338d72c578a61138dc496def1438ff5
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-11 12:38:46 -0600

    feat: add SessionDetailNewVersion component with styling and functionality for submitting new session versions

---
+++ components/session/SessionDetailNewVersion/SessionDetailNewVersion.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { useMemo } from "react";
+
+import "./SessionDetailNewVersion.scss";
+
+interface SessionDetailNewVersionProps {
+  sessionID?: string;
+  versionID?: string;
+}
+
+export default function SessionDetailNewVersion({
+  sessionID,
+  versionID,
+}: SessionDetailNewVersionProps) {
+  const linkTo = useMemo(() => {
+    if (!sessionID) return "/sessions";
+    const params = new URLSearchParams();
+    if (versionID) {
+      params.set("versionID", versionID);
+    }
+    const queryString = params.toString();
+    return `/sessions/${sessionID}/new${queryString ? `?${queryString}` : ""}`;
+  }, [sessionID, versionID]);
+
+  return (
+    <div className="session-detail-new-version">
+      <div className="new-version-title">Versions</div>
+      <Link href={linkTo} className="btn new-version">
+        + Submit New Version
+      </Link>
+    </div>
+  );
+}
