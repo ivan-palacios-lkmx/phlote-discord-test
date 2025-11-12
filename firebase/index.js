628f213441ccdd0bac9db96a9951062ff53a6557
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-12 15:52:08 -0600

    feat: add Firebase initialization and functions setup, including service account integration and package updates for improved functionality

---
+++ firebase/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+require("./initialize");
+
+const functions = require("firebase-functions");
+const firebase = require("firebase-admin");
+
+
