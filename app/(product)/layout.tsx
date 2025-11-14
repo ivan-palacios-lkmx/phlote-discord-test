95fb120b437bffe9a259ab19320e07d1d162ddc5
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-14 11:30:39 -0600

    refactor: simplify ProductLayout by removing OnlyMembers wrapper and updating SessionsPageClient to utilize OnlyMembers for access control

--- app/(product)/layout.tsx
+++ app/(product)/layout.tsx
@@ -3,9 +3,5 @@ import OnlyMembers from "@/components/OnlyMembers/OnlyMembers";
 import "./layout.scss";
 
 export default async function ProductLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="page">
-      <OnlyMembers>{children}</OnlyMembers>
-    </main>
-  );
+  return <>{children}</>;
 }
