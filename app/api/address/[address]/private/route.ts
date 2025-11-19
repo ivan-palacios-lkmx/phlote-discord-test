e043960fdcbbef61bb3ed5b57004ddbdc4e5e292
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-19 11:55:33 -0600

    refactor: update GET and PUT endpoints to use route parameters for address extraction, improving code clarity and maintainability

--- app/api/address/[address]/private/route.ts
+++ app/api/address/[address]/private/route.ts
@@ -2,9 +2,12 @@ import { AddressService } from "@/services/address-service";
 import { addressSchema, contactSchema } from "@/utils/zod-schemas";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest): Promise<NextResponse> {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { address: string } },
+): Promise<NextResponse> {
   try {
-    const address = request.nextUrl.pathname.split("/").pop();
+    const address = params.address;
 
     if (!addressSchema.safeParse(address).success) {
       return NextResponse.json({ error: "Invalid address format" }, { status: 400 });
@@ -18,16 +21,19 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   }
 }
 
-export async function PUT(request: NextRequest): Promise<NextResponse> {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { address: string } },
+): Promise<NextResponse> {
   try {
-    const address = request.nextUrl.pathname.split("/").pop();
+    const address = params.address;
     const contact = await request.json();
 
     if (!contactSchema.safeParse(contact).success) {
       return NextResponse.json({ error: "Invalid contact format" }, { status: 400 });
     }
 
-    await AddressService.updatePrivateAddressData(address!, contact);
+    await AddressService.updatePrivateAddressData(address, contact);
     return NextResponse.json({ message: "Contact updated" }, { status: 200 });
   } catch (error) {
     console.error("Error updating address private data:", error);
