68826eee33b657ecaaa037ff702a5a92c57d7db6
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-19 11:56:16 -0600

    refactor: modify GET endpoint to utilize route parameters for address extraction, enhancing code clarity and maintainability

--- app/api/address/[address]/route.ts
+++ app/api/address/[address]/route.ts
@@ -2,19 +2,22 @@ import { AddressService } from "@/services/address-service";
 import { addressSchema } from "@/utils/zod-schemas";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest): Promise<NextResponse> {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { address: string } },
+): Promise<NextResponse> {
   try {
     const searchParams = request.nextUrl.searchParams;
 
     const includePrivate = searchParams.get("include") === "private";
 
-    const address = request.nextUrl.pathname.split("/").pop();
+    const address = params.address;
 
     if (!addressSchema.safeParse(address).success) {
       return NextResponse.json({ error: "Invalid address format" }, { status: 400 });
     }
 
-    const addressDoc = await AddressService.getSingleAddress(address!, includePrivate);
+    const addressDoc = await AddressService.getSingleAddress(address, includePrivate);
 
     if (!addressDoc) {
       return NextResponse.json({ error: "Address not found" }, { status: 404 });
