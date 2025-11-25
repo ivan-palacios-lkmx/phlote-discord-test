69c89b4b07e9ec26fae3f18c63ef8e03ca55a646
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-24 22:42:02 -0600

    refactor: Simplify OnlyAdmins component by removing unused styles and implementing custom hooks for authentication and wallet connection

--- components/OnlyAdmins/OnlyAdmins.tsx
+++ components/OnlyAdmins/OnlyAdmins.tsx
@@ -3,6 +3,7 @@
 import LoadingSpinnerIcon from "@/components/svg/loading_spinner.svg";
 import { useGetAddressInfo } from "@/hooks/query/query-hooks/use-get-address-info";
 import { useLogin, usePrivy } from "@privy-io/react-auth";
+import { useMemo } from "react";
 
 import "./OnlyAdmins.scss";
 
@@ -11,50 +12,68 @@ interface OnlyAdminsProps {
   className?: string;
 }
 
-export default function OnlyAdmins({ children, className }: OnlyAdminsProps) {
+function useFbAuth() {
+  // TODO: Implement useFbAuth hook properly
+  // This should return userDoc and loadingUser similar to Vue version
+  const { ready, authenticated, user } = usePrivy();
+
+  const { data: userDoc, isPending } = useGetAddressInfo(
+    user?.wallet?.address || "",
+    !!user?.wallet?.address && authenticated,
+  );
+
+  const loadingUser = useMemo(() => {
+    return !ready || (authenticated && isPending);
+  }, [ready, authenticated, isPending]);
+
+  return { userDoc, loadingUser };
+}
+
+function useWeb3Modal() {
+  // TODO: Implement useWeb3Modal hook
+  // This should return a ref with openModal method
   const { login } = useLogin();
-  const { authenticated, logout, ready, user } = usePrivy();
 
-  const {
-    data: addressDoc,
-    isPending,
-    isError,
-  } = useGetAddressInfo(user?.wallet?.address || "", !!user?.wallet?.address && authenticated);
+  return useMemo(
+    () => ({
+      value: {
+        openModal: () => {
+          login();
+        },
+      },
+    }),
+    [login],
+  );
+}
 
-  const isAdmin = authenticated && addressDoc?.isAdmin;
+export default function OnlyAdmins({ children, className }: OnlyAdminsProps) {
+  const { userDoc, loadingUser } = useFbAuth();
+  const web3modal = useWeb3Modal();
+
+  const isMember = useMemo(() => {
+    return !!(userDoc as { isAdmin?: boolean } | null)?.isAdmin;
+  }, [userDoc]);
 
   const onConnect = () => {
-    if (authenticated) {
-      logout();
-    }
-    login();
+    web3modal.value.openModal();
   };
 
   return (
-    <main className={`only-admins ${className}`}>
-      {(isPending && authenticated) || !ready ? (
+    <main className={`only-admins ${className || ""}`}>
+      {loadingUser ? (
         <div className="only-admins-loading">
           <LoadingSpinnerIcon />
         </div>
-      ) : isAdmin ? (
+      ) : isMember ? (
         <div className="only-admins-revealed">{children}</div>
       ) : (
         <div className="only-admins-locked">
           <div className="contained">
             <div className="centered">
-              <h4>This content is for admins only.</h4>
+              <h4>This area is for admins only</h4>
               <button onClick={onConnect} className="btn">
                 Connect Wallet
               </button>
-            </div>
-          </div>
-        </div>
-      )}
-      {isError && (
-        <div className="only-admins-error">
-          <div className="contained">
-            <div className="centered">
-              <h4>Error loading user data.</h4>
             </div>
           </div>
         </div>
