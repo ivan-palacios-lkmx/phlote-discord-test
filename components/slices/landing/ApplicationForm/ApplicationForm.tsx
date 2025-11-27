fa1373db2fb1a00be5d61c0489f5129ce461741e
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-27 11:16:43 -0600

    refactor: Remove local state management from ApplicationForm and simplify form submission logic

--- components/slices/landing/ApplicationForm/ApplicationForm.tsx
+++ components/slices/landing/ApplicationForm/ApplicationForm.tsx
@@ -9,7 +9,6 @@ import { useCreateCreatorApplication } from "@/hooks/query/mutations/use-create-
 import type { ApplicationFormSlice } from "@/types/client";
 import { applicationFormSchema } from "@/utils/zod-schemas";
 import type { SliceComponentProps } from "@prismicio/react";
-import { useMemo, useState } from "react";
 import { z } from "zod";
 
 import "./ApplicationForm.scss";
@@ -17,41 +16,12 @@ import "./ApplicationForm.scss";
 export default function ApplicationForm({
   slice: _slice, // eslint-disable-line @typescript-eslint/no-unused-vars
 }: SliceComponentProps<ApplicationFormSlice>) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [city, setCity] = useState("");
-  const [info, setInfo] = useState("");
-  const [workLink, setWorkLink] = useState("");
-  const [ethAddress, setEthAddress] = useState("");
-  const [multiTrackUploadHasFiles, setMultiTrackUploadHasFiles] = useState(false);
-  const [multiTrackUploadHasError, setMultiTrackUploadHasError] = useState(false);
   const {
     mutate: createCreatorApplication,
     error: createCreatorApplicationError,
     isPending: isCreatingCreatorApplication,
     isSuccess: isCreatorApplicationCreated,
   } = useCreateCreatorApplication();
-
-  const canSubmit = useMemo(
-    () =>
-      firstName &&
-      lastName &&
-      email &&
-      city &&
-      ethAddress &&
-      multiTrackUploadHasFiles &&
-      !multiTrackUploadHasError,
-    [
-      firstName,
-      lastName,
-      email,
-      city,
-      ethAddress,
-      multiTrackUploadHasFiles,
-      multiTrackUploadHasError,
-    ],
-  );
 
   function handleSubmit(formValues: z.infer<typeof applicationFormSchema>) {
     console.log(formValues);
@@ -77,8 +47,6 @@ export default function ApplicationForm({
             <span>First Name*</span>
             <Input
               name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
               placeholder="John"
               type="text"
               maxLength={100}
@@ -91,8 +59,6 @@ export default function ApplicationForm({
             <span>Last Name*</span>
             <Input
               name="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
               placeholder="Doe"
               type="text"
               maxLength={100}
@@ -105,8 +71,6 @@ export default function ApplicationForm({
             <span>Email Address*</span>
             <Input
               name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               placeholder="name@example.com"
               type="email"
               maxLength={100}
@@ -119,8 +83,6 @@ export default function ApplicationForm({
             <span>City*</span>
             <Input
               name="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
               placeholder="Los Angeles"
               type="text"
               maxLength={100}
@@ -133,8 +95,6 @@ export default function ApplicationForm({
             <span>Additional Information</span>
             <Textarea
               name="info"
-              value={info}
-              onChange={(e) => setInfo(e.target.value)}
               id="info"
               maxLength={500}
               disabled={isCreatorApplicationCreated}
@@ -148,8 +108,6 @@ export default function ApplicationForm({
             <span>Link to Your Work</span>
             <Input
               name="workLink"
-              value={workLink}
-              onChange={(e) => setWorkLink(e.target.value)}
               placeholder="https://my-portfolio.com"
               type="url"
               maxLength={100}
@@ -161,8 +119,6 @@ export default function ApplicationForm({
             <span>Wallet Address / ENS*</span>
             <Input
               name="ethAddress"
-              value={ethAddress}
-              onChange={(e) => setEthAddress(e.target.value)}
               placeholder="phlote.eth"
               type="text"
               maxLength={100}
@@ -178,7 +134,7 @@ export default function ApplicationForm({
         </div>
 
         <div className="button-row">
-          <button className="btn" type="submit" disabled={!canSubmit}>
+          <button className="btn" type="submit">
             {isCreatingCreatorApplication ? (
               <LoadingSpinnerIcon className="loading-spinner" />
             ) : isCreatorApplicationCreated ? (
