6d6c3b1fcf10f475bd294616c11cef9514178bbd
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-27 13:19:08 -0600

    feat: Add onWatch prop to Input and Textarea components for real-time value tracking

--- components/Form/Input/Input.tsx
+++ components/Form/Input/Input.tsx
@@ -1,14 +1,24 @@
-import { InputHTMLAttributes } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import { InputHTMLAttributes, useEffect } from "react";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
 
 import "./Input.scss";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
+  onWatch?: (formValues: Record<string, unknown>) => void;
 }
 
-export function Input({ name, ...props }: InputProps) {
+export function Input({ name, onWatch, ...props }: InputProps) {
   const { control } = useFormContext();
+
+  const value = useWatch({ control, name });
+
+  useEffect(() => {
+    if (onWatch) {
+      onWatch({ [name]: value });
+    }
+  }, [value, name, onWatch]);
+
   return (
     <Controller
       control={control}
--- components/Form/Textarea.tsx
+++ components/Form/Textarea.tsx
@@ -1,12 +1,21 @@
-import { TextareaHTMLAttributes } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import { TextareaHTMLAttributes, useEffect } from "react";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string;
+  onWatch?: (formValue: Record<string, unknown>) => void;
 }
 
-export function Textarea({ name, ...props }: TextareaProps) {
+export function Textarea({ name, onWatch, ...props }: TextareaProps) {
   const { control } = useFormContext();
+  const value = useWatch({ control, name });
+
+  useEffect(() => {
+    if (onWatch) {
+      onWatch({ [name]: value });
+    }
+  }, [value, name, onWatch]);
+
   return (
     <Controller
       control={control}
