099560513e16882ac9a7f7069f1dcc4705b8fc5e
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-27 16:32:49 -0600

    fix: Correct stroke property in Close icon component for proper rendering

--- components/icons/Close.tsx
+++ components/icons/Close.tsx
@@ -9,8 +9,8 @@ const Close = (props: React.SVGProps<SVGSVGElement>) => {
       viewBox="0 0 30 31"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <path d="M2.05078 2.85205L27.9508 28.7521" stroke="currentColor" stroke-width="3" />
-      <path d="M27.9512 2.85205L2.05117 28.7521" stroke="currentColor" stroke-width="3" />
+      <path d="M2.05078 2.85205L27.9508 28.7521" strokeWidth="currentColor" stroke-width="3" />
+      <path d="M27.9512 2.85205L2.05117 28.7521" strokeWidth="currentColor" stroke-width="3" />
     </svg>
   );
 };
