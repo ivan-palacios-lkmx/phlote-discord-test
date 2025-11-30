d915ba586f12f3b8119e9546afc239706998e745
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-30 14:28:43 -0600

    feat: Wrap NewSessionPage in OnlyCreators component to enforce role-based access control

--- app/(product)/sessions/new/page.tsx
+++ app/(product)/sessions/new/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import NewProjectForm from "@/components/NewProjectForm/NewProjectForm";
+import OnlyCreators from "@/components/OnlyCreators/OnlyCreators";
 
 import "./page.scss";
 
 export default function NewSessionPage() {
   return (
-    <main className="new-session">
+    <OnlyCreators className="new-session">
       <div className="contained">
         <NewProjectForm type="session" />
       </div>
-    </main>
+    </OnlyCreators>
   );
 }
