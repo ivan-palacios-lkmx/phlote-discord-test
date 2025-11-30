5b53eb7e740c6ab7b70a0df505af1fd09118ced8
ivan.palacios <ivan.palacios@lkmx.io>
2025-11-30 17:41:29 -0600

    feat: Implement POST endpoint for application submission and validation, while removing deprecated creator applications route

--- app/api/applications/route.ts
+++ app/api/applications/route.ts
@@ -1,5 +1,6 @@
 import { ApplicationService } from "@/services/application-service";
-import { NextResponse } from "next/server";
+import { applicationFormSchema } from "@/utils/zod-schemas";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse> {
   try {
@@ -7,6 +8,35 @@ export async function GET(): Promise<NextResponse> {
     return NextResponse.json(applications, { status: 200 });
   } catch (error) {
     console.error("Error getting applications:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { firstName, lastName, email, city, info, workLink, ethAddress, tracks } = body;
+
+  if (
+    !applicationFormSchema.safeParse({
+      firstName,
+      lastName,
+      email,
+      city,
+      info,
+      workLink,
+      ethAddress,
+      tracks,
+    }).success
+  ) {
+    return NextResponse.json({ error: "Invalid application" }, { status: 400 });
+  }
+
+  try {
+    const applicationId = await ApplicationService.createApplication(body);
+
+    return NextResponse.json({ applicationId }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating application:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
