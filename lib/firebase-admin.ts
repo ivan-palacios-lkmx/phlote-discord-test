6523c1c134b85cbcdd1e856fc8b3cfec68ea3488
ivan.palacios <ivan.palacios@lkmx.io>
2025-12-01 14:43:12 -0600

    refactor: Add TODO comment to prevent multiple initializations of Firebase Admin app and remove unnecessary console logs

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,16 +1,15 @@
 import serviceAccount from "@/firebase/service-account.json";
 import admin from "firebase-admin";
 
+// TODO: Prevent multiple initializations of the admin app
 if (!admin.apps.length) {
   if (serviceAccount) {
     const projectID = serviceAccount.project_id;
-    console.log("[firebase-admin] Project ID:", projectID);
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
       databaseURL: `https://${projectID}.firebaseio.com`,
       storageBucket: `${projectID}.appspot.com`,
     });
-    console.log("[firebase-admin] Firebase Admin initialized with service account");
   }
 }
 
