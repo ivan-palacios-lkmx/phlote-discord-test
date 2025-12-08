cc19865a88f56ab8788760195250c13f4a82a005
Hugo Corona <hugo.corona@lkmx.io>
2025-12-08 14:35:09 -0800

    fix: Update button text from 'Logout' to 'Log out' for consistency in user interface

--- components/OverlayProfile/SessionsLink/SessionsLink.tsx
+++ components/OverlayProfile/SessionsLink/SessionsLink.tsx
@@ -42,7 +42,7 @@ function SessionsLinkContent({ profileID, onClose }: SessionsLinkProps) {
         My Sessions
       </button>
       <button type="button" onClick={onDisconnect}>
-        Logout
+        Log out
       </button>
     </div>
   );
